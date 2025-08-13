427a46abebc1e869416d972056279a27afa81760
Souzaa1 <brpasa@yahoo.com.br>
2025-08-13 08:57:20 -0300

    feat: merge items

--- components/ui/nav-main.tsx
+++ components/ui/nav-main.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { ChevronRight, type LucideIcon } from "lucide-react"
-
 import {
     Collapsible,
     CollapsibleContent,
@@ -17,6 +16,7 @@ import {
     SidebarMenuSubItem,
 } from '@/components/ui/sidebar'
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export function NavMain({
     items,
@@ -29,11 +29,12 @@ export function NavMain({
         items?: {
             title: string
             url: string
-            isActive?: boolean
             icon?: LucideIcon
         }[]
     }[]
 }) {
+    const pathname = usePathname();
+
     return (
         <SidebarGroup>
             <SidebarMenu>
@@ -46,10 +47,7 @@ export function NavMain({
                     >
                         <SidebarMenuItem>
                             <CollapsibleTrigger asChild>
-                                <SidebarMenuButton
-                                    tooltip={item.title}
-                                    className={item.isActive ? "bg-zinc-600 text-zinc-300" : ""}
-                                >
+                                <SidebarMenuButton tooltip={item.title} className={item.isActive ? "bg-zinc-800 text-zinc-500 font-semibold rounded-full" : ""}>
                                     {item.icon && <item.icon />}
                                     <span>{item.title}</span>
                                     <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
@@ -57,16 +55,22 @@ export function NavMain({
                             </CollapsibleTrigger>
                             <CollapsibleContent>
                                 <SidebarMenuSub>
-                                    {item.items?.map((subItem) => (
-                                        <SidebarMenuSubItem key={subItem.title} className={subItem.isActive ? "bg-zinc-600 text-zinc-300 rounded-full" : ""}>
-                                            <SidebarMenuSubButton asChild>
-                                                <Link href={subItem.url}>
-                                                    {subItem.icon && <subItem.icon />}
-                                                    <span>{subItem.title}</span>
-                                                </Link>
-                                            </SidebarMenuSubButton>
-                                        </SidebarMenuSubItem>
-                                    ))}
+                                    {item.items?.map((subItem) => {
+                                        const isActive = pathname === subItem.url;
+                                        return (
+                                            <SidebarMenuSubItem key={subItem.title}>
+                                                <SidebarMenuSubButton
+                                                    asChild
+                                                    className={isActive ? "bg-zinc-800 text-zinc-500 font-semibold rounded-full" : ""}
+                                                >
+                                                    <Link href={subItem.url}>
+                                                        {subItem.icon && <subItem.icon />}
+                                                        <span>{subItem.title}</span>
+                                                    </Link>
+                                                </SidebarMenuSubButton>
+                                            </SidebarMenuSubItem>
+                                        )
+                                    })}
                                 </SidebarMenuSub>
                             </CollapsibleContent>
                         </SidebarMenuItem>
